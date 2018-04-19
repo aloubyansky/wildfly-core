4bfbfa3accd47b98217dc266223976fea60dc45c
Tomas Hofman <thofman@redhat.com>
2018-04-19 11:18:40 +0200

    failing assert when dynamicRequirementNameMapper not set...
    ...in resource requirement definition, RuntimeCapability#buildDynamicCapabilityName() line 65

--- controller/src/main/java/org/jboss/as/controller/operations/global/ReadFeatureHandler.java
+++ controller/src/main/java/org/jboss/as/controller/operations/global/ReadFeatureHandler.java
@@ -786,7 +786,7 @@ public class ReadFeatureHandler extends GlobalOperationHandlers.AbstractMultiTar
                     for (CapabilityReferenceRecorder requirement : registration.getRequirements()) {
                         String[] segments = requirement.getRequirementPatternSegments(null, aliasAddress);
                         String[] dynamicElements;
-                        if (segments == null) {
+                        if (segments == null || segments.length == 0) {
                             dynamicElements = null;
                         } else {
                             dynamicElements = new String[segments.length];
